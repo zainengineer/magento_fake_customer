af638b302e15fed644f293259331253f0b6132d5
Zain ul abidin <zain@aligent.com.au>
2018-09-10 13:06:10 +0930

    magneto: basic customer generator and added docker

--- app/Magento/Customer/Main.php
+++ app/Magento/Customer/Main.php
@@ -1,10 +1,30 @@
 <?php
 namespace App\Magento\Customer;
 
+use Faker\Factory as FakerFactory;
+use Faker\Generator as FakerGenerator;
 class Main
 {
-    public function test()
+    protected $fakerCached;
+    protected function getFakerGenerator() : FakerGenerator
     {
-        return 'Hello World';
+        if (!$this->fakerCached){
+            $this->fakerCached = FakerFactory::create();;
+        }
+        return $this->fakerCached;
+
+    }
+    public function getOneCustomer() : array
+    {
+        $faker = $this->getFakerGenerator();
+        $customer = [
+            'firstname' => $faker->firstName,
+            'lastname' => $faker->lastName,
+            'email' => $faker->email,
+            'created_at' => $faker->date(),
+            'gender' => rand(0,1) ? 'Male': 'Female',
+        ];
+        return $customer;
+
     }
 }
--- magento.php
+++ magento.php
@@ -3,4 +3,5 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/fatal_error.php';
 
 $customer = new \App\Magento\Customer\Main();
-var_dump($customer->test());
+var_dump($customer->getOneCustomer());
+var_dump($customer->getOneCustomer());
